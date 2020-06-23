3bef5b20ed5304c924a9e7230f4a7af658933e6d
Eraldo Henrique Maia <55844931+EraldoMaia@users.noreply.github.com>
2020-06-23 09:54:49 -0300

    tela de login

--- src/screens/Auth.js
+++ src/screens/Auth.js
@@ -1,14 +1,51 @@
 import React, {Component} from 'react'
-import {ImageBackground, Text, StyleSheet} from 'react-native'
+import {ImageBackground, Text, StyleSheet, View, TextInput,
+        TouchableOpacity,
+        Platform,
+        } from 'react-native'
 
 import commonStyles from '../commonStyles'
 import backgroundImage from '../../assets/imgs/login.jpg'
 
 export default class Auth extends Component{
+  state = {
+      name: '',
+      email: '',
+      password: '',
+      confirmPassword: '',
+      stageNew: true,
+  }
+
   render(){
     return(
         <ImageBackground source={backgroundImage} style={styles.backgroud}>
           <Text style={styles.title}>TASKS</Text>
+          <View style={styles.formConteiner}>
+            <Text style={styles.subtitle}>
+            {
+              this.state.stageNew ? 'Crie seu acesso' : 'Informe seus dados'
+            }
+            </Text>
+            {this.state.stageNew && 
+            <TextInput placeholder='Nome' value={this.state.name} style={styles.input}
+            onChangeText={name => this.setState({name})}/>
+            }
+            <TextInput placeholder='E-mail' value={this.state.email} style={styles.input}
+            onChangeText={email => this.setState({email})}/>
+            <TextInput placeholder='Senha' value={this.state.password} style={styles.input}
+            onChangeText={password => this.setState({password})} secureTextEntry={true}/>
+            {this.state.stageNew && 
+            <TextInput placeholder='Confirme a Senha' value={this.state.confirmPassword} style={styles.input}
+            onChangeText={confirmPassword=> this.setState({confirmPassword})} secureTextEntry={true}/>
+            }
+            <TouchableOpacity>
+              <View style={styles.button}>
+                <Text style={styles.buttonText}>{
+                  this.state.stageNew ? 'Cadastrar' : 'Acessar'
+                }</Text>
+              </View>
+            </TouchableOpacity>
+          </View>
         </ImageBackground>
     )
   }
@@ -27,4 +64,33 @@ const styles = StyleSheet.create({
     fontSize: 70,
     marginBottom: 10,
   },
+  subtitle:{
+    fontFamily: commonStyles.fontFamily,
+    color: '#fff',
+    fontSize: 20,
+    textAlign: 'center',
+    marginBottom: 10,
+  },
+  input:{
+    backgroundColor: '#fff',
+    marginTop: 10,
+    padding: Platform.OS == 'ios' ? 15 : 10
+  },
+  formConteiner: {
+    backgroundColor: 'rgba(0,0,0,0.8)',
+    padding: 20,
+    width: '90%',
+  },
+  button:{
+    backgroundColor: '#080',
+    marginTop: 10,
+    padding: 10,
+    alignItems: 'center',
+  },
+  buttonText:{
+    fontFamily: commonStyles.fontFamily,
+    color: '#FFF',
+    fontSize: 20,
+  },
+  
 })
