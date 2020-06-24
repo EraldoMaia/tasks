ac88e7464558debb621a68c8ced8af5eab64a704
Eraldo Henrique Maia <55844931+EraldoMaia@users.noreply.github.com>
2020-06-24 17:05:33 -0300

    deleta e insere do bd

--- src/screens/Auth.js
+++ src/screens/Auth.js
@@ -1,31 +1,79 @@
 import React, {Component} from 'react'
-import {ImageBackground, Text, StyleSheet, View, TextInput,
+import {ImageBackground, Text, StyleSheet, View,
         TouchableOpacity,
-        Platform,
         Alert,
         } from 'react-native'
+import axios from 'axios'
 
 import commonStyles from '../commonStyles'
 import backgroundImage from '../../assets/imgs/login.jpg'
+import AuthInput from '../components/Authinput'
+import {server, showError, showSuccess} from '../common'
+
+const initialState = {
+      name: '',
+      email: 'eraldomaia.mec@outlook.com',
+      password: '123456',
+      confirmPassword: '',
+      stageNew: false,
+}
 
 export default class Auth extends Component{
   state = {
-      name: '',
-      email: '',
-      password: '',
-      confirmPassword: '',
-      stageNew: false,
-  }
+    ...initialState
+}
 
-  siginOrSignup = () => {
-    if (this,state.stageNew){
-      Alert.alert('Sucesso!', 'Criar conta!')
-    }else{
-      Alert.alert('Sucesso!', 'Logar!')
+signinOrSignup = () => {
+  if(this.state.stageNew) {
+      this.signup()
+    }else
+      {
+      this.signin()
     }
   }
 
+  signup = async () => {
+    try {
+        await axios.post(`${server}/signup`, {
+            name: this.state.name,
+            email: this.state.email,
+            password: this.state.password,
+            confirmPassword: this.state.confirmPassword,
+        })
+
+        showSuccess('Usuário cadastro!')
+        this.setState({ ...initialState })
+    } catch(e) {
+        showError(e)
+    }
+}
+
+signin = async () => {
+  try {
+   const res = await axios.post(`${server}/signin`, {
+      email: this.state.email,
+      password: this.state.password,
+  })
+    axios.defaults.headers.common['Authorization'] = `bearer ${res.data.token}`
+    this.props.navigation.navigate('Home')
+  }
+  catch(e){
+    showError(e)
+  }
+}
+
   render(){
+    const validations = []
+    validations.push(this.state.email && this.state.email.includes('@'))
+    validations.push(this.state.password && this.state.password.length >= 6)
+
+    if(this.state.stageNew)
+    { 
+      validations.push(this.state.name && this.state.name.trim().length >= 3)
+      validations.push(this.state.password === this.state.confirmPassword)
+    }
+    const validForm = validations.reduce((t,a) => t && a)
+
     return(
         <ImageBackground source={backgroundImage} style={styles.backgroud}>
           <Text style={styles.title}>TASKS</Text>
@@ -36,19 +84,20 @@ export default class Auth extends Component{
             }
             </Text>
             {this.state.stageNew && 
-            <TextInput placeholder='Nome' value={this.state.name} style={styles.input}
+            <AuthInput icon='user' placeholder='Nome' value={this.state.name} style={styles.input}
             onChangeText={name => this.setState({name})}/>
             }
-            <TextInput placeholder='E-mail' value={this.state.email} style={styles.input}
+            <AuthInput icon='at' placeholder='E-mail' value={this.state.email} style={styles.input}
             onChangeText={email => this.setState({email})}/>
-            <TextInput placeholder='Senha' value={this.state.password} style={styles.input}
+            <AuthInput icon='lock' placeholder='Senha' value={this.state.password} style={styles.input}
             onChangeText={password => this.setState({password})} secureTextEntry={true}/>
             {this.state.stageNew && 
-            <TextInput placeholder='Confirme a Senha' value={this.state.confirmPassword} style={styles.input}
+            <AuthInput icon='asterisk' placeholder='Confirme a Senha' value={this.state.confirmPassword} style={styles.input}
             onChangeText={confirmPassword=> this.setState({confirmPassword})} secureTextEntry={true}/>
             }
-            <TouchableOpacity onPress={this.siginOrSignup}>
-              <View style={styles.button}>
+            <TouchableOpacity onPress={this.signinOrSignup}
+            disabled={!validForm}>
+              <View style={[styles.button, validForm ? {} : {backgroundColor: '#aaa'}]}>
                 <Text style={styles.buttonText}>{this.state.stageNew ? 'Cadastrar' : 'Acessar'}</Text>
               </View>
             </TouchableOpacity>
@@ -87,18 +136,19 @@ const styles = StyleSheet.create({
   input:{
     backgroundColor: '#fff',
     marginTop: 10,
-    padding: Platform.OS == 'ios' ? 15 : 10
   },
   formConteiner: {
     backgroundColor: 'rgba(0,0,0,0.8)',
     padding: 20,
     width: '90%',
+    borderRadius: 10,
   },
   button:{
     backgroundColor: '#080',
     marginTop: 10,
     padding: 10,
     alignItems: 'center',
+    borderRadius: 10,
   },
   buttonText:{
     fontFamily: commonStyles.fontFamily,
--- src/screens/TaskList.js
+++ src/screens/TaskList.js
@@ -7,18 +7,19 @@ import {
   FlatList,
   TouchableOpacity,
   Platform,
-  TouchableOpacityBase,
   Alert,
 } from 'react-native';
 import moment from 'moment';
 import 'moment/locale/pt-br';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import axios from 'axios';
 import AsyncStorage from '@react-native-community/async-storage'
 
 import todayImage from '../../assets/imgs/today.jpg';
 import commonStyles from '../commonStyles';
 import Task from '../components/Task';
 import AddTask from './AddTask';
+import { server, showError} from '../common'
 
 const inicialstate = {
   showDoneTasks: true,
@@ -33,9 +34,21 @@ export default class TaskList extends Component {
   };
   componentDidMount = async () => {
     const stateString = await AsyncStorage.getItem('tasksState')
-    const state = JSON.parse(stateString) || inicialstate
-    this.setState(state, this.filterTasks)
+    const savedState = JSON.parse(stateString) || inicialstate
+    this.setState({showDoneTasks: savedState.showDoneTasks}, this.filterTasks)
+
+    this.loadTasks()
   };
+
+  loadTasks = async () => {
+    try{
+      const maxDate = moment().format('YYYY-MM-DD 23:59:59')
+      const res = await axios.get(`${server}/tasks?date=${maxDate}`)
+      this.setState({tasks: res.data}, this.filterTasks)
+    }catch(e){
+      showError(e)
+    }
+  }
 
   toggleFilter = () => {
     this.setState({showDoneTasks: !this.state.showDoneTasks}, this.filterTasks);
@@ -50,37 +63,45 @@ export default class TaskList extends Component {
       visibleTasks = this.state.tasks.filter(pending);
     }
     this.setState({visibleTasks});
-    AsyncStorage.setItem('tasksState',JSON.stringify(this.state),)
+    AsyncStorage.setItem('tasksState',JSON.stringify({
+      showDoneTasks: this.state.showDoneTasks
+    }),)
   };
-  toggleTask = taskId => {
-    const tasks = [...this.state.tasks];
-    tasks.forEach(task => {
-      if  (task.id === taskId) {
-        task.doneAt = task.doneAt ? null : new Date();
+  toggleTask = async taskId => {
+      try{
+        await axios.put(`${server}/tasks/${taskId}/toggle`)
+        this.loadTasks()
+      }catch(e){
+        showError(e)
       }
-    });
-    this.setState({tasks}, this.filterTasks)
   };
 
-  addTask = newTask => {
-    if(!newTask.desc || !newTask.desc.trim()){
-      Alert.alert('Dados invalidos!', 'Descrição não informada!')
-      return
+  addTask = async newTask => {
+    if(!newTask.desc || !newTask.desc.trim()) {
+        Alert.alert('Dados Inválidos', 'Descrição não informada!')
+        return 
     }
-    const tasks = [...this.state.tasks]
-    tasks.push({
-      id: Math.random(),
-      desc: newTask.desc,
-      estimateAt: newTask.date,
-      doneAt: null,
-    })
 
-    this.setState({tasks, showAddTask: false}, this.filterTasks)
-  }
+    try {
+        await axios.post(`${server}/tasks`, {
+           desc: newTask.desc,
+           estimateAt: newTask.date 
+        })
 
-  deleteTask = (id) =>{
-    const tasks = this.state.tasks.filter(tasks => tasks.id !== id)
-    this.setState({tasks}, this.filterTasks)
+        this.setState({ showAddTask: false }, this.loadTasks)
+    } catch(e) {
+        showError(e)
+    }
+}
+
+
+  deleteTask = async (taskId) =>{
+    try{
+      await axios.delete(`${server}/tasks/${taskId}`)
+      this.loadTasks()
+    }catch(e){
+      showError(e)
+    }
   }
 
   render() {
